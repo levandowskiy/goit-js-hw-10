a66dde1b63d784440bfe3417d329f21736de817c
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2023-05-24 14:31:10 +0300

    fix bugs

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,15 @@
 const BASE_URL = "https://restcountries.com/v3.1"
 
 export function fetchCountries(name) {
-    return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
+  return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-    });   
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
 }
 
 
 
 
- 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import {fetchCountries} from './fetchCountries'
+import { fetchCountries } from './fetchCountries'
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const debounce = require('lodash.debounce');
@@ -15,38 +15,40 @@ function handleCountrySearchInput() {
     const inputValue = inputSearch.value.trim();
     const isEmptyInput = inputValue === "";
 
-    if(isEmptyInput){  
+    if (isEmptyInput) {
         countryList.innerHTML = "";
         countryInfo.innerHTML = "";
         return
     }
 
     fetchCountries(inputValue)
-    .then(countries => {
-        const isValidRequest = checkNumberCountries(countries);
+        .then(countries => {
+            const isValidRequest = checkNumberCountries(countries);
 
-        if(isValidRequest){
-            makeCountryListMarkup(countries);
-        }
+            if (isValidRequest) {
+                makeCountryListMarkup(countries);
+            }
 
-        const foundCountry = countries.length === 1;
-        
-        if(foundCountry){
-            countryList.innerHTML = "";
-            countryInfo.innerHTML = makeCardMarkup(countries[0])
-        }
-    })
-    .catch(error => {
-        if(error.message === "404") {
+            const foundCountry = countries.length === 1;
+
+            if (foundCountry) {
+                countryList.innerHTML = "";
+                countryInfo.innerHTML = makeCardMarkup(countries[0])
+            }
+        })
+        .catch(error => {
             countryList.innerHTML = "";
             countryInfo.innerHTML = "";
-            Notiflix.Notify.failure("Oops, there is no country with that name"); 
-        }
-    });
+            if (error.message === "404") {
+                Notiflix.Notify.failure("Oops, there is no country with that name");
+            } else {
+                Notiflix.Notify.failure(error.message);
+            }
+        });
 }
 
 function checkNumberCountries(countries) {
-    if(countries.length>10){
+    if (countries.length > 10) {
         Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
         countryList.innerHTML = "";
         countryInfo.innerHTML = "";
@@ -65,14 +67,14 @@ function makeCountryListMarkup(countries) {
 
         countryflagItem.setAttribute("src", country.flags.svg);
         countryflagItem.classList.add("flag-img")
-        countryName.textContent = country.name.common; 
-       
+        countryName.textContent = country.name.common;
+
         countryItem.appendChild(countryflagItem);
         countryItem.appendChild(countryName);
-        
+
         return countryItem;
-        });
-    
+    });
+
     countryList.append(...countriesItems)
 }
 
@@ -81,7 +83,7 @@ function makeCardMarkup(country) {
         <li class="country-card-item"><img src="${country.flags.svg}"><h2 class="card-title">${country.name.common}</h2></li>
         <li class="country-card-item"><h3>Capital:</h2><p>${country.capital}</p></li>
         <li class="country-card-item"><h3>Population:</h2><p>${country.population}<p/></li>
-        <li class="country-card-item"><h3>Languages:</h2><p>${Object.values(country.languages)}</p></li>
+        <li class="country-card-item"><h3>Languages:</h2><p>${Object.values(country.languages).join(", ")}</p></li>
         </ul>`;
 }
 
